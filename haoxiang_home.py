5668974c542615f5b27ff230dd8bc9e424f2c827
higherstep <15304403010@163.com>
2024-08-10 20:14:13 +0800

    Update haoxiang_home.py

--- haoxiang_home.py
+++ haoxiang_home.py
@@ -18,7 +18,6 @@ def page1():
     with open('八年五班《大香蕉》伴奏.MP3','rb') as f:
         mymp3=f.read()
         st.audio(mymp3, format='audio/mp3', start_time=0)
-        st.image('bing_image.jpg')
         st.image('bing_image_1.png')
         st.image('bing_image_2.png')
         st.image('bing_image_3.png')
